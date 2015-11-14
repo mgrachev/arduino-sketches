479075db7bb0825b715a693d4d2d53182e28f254
Mikhail Grachev <work@mgrachev.com>
2015-11-14 13:35:54 +0300

    Update sketch 01

--- 01_1_beacon/src/example.ino
+++ 01_1_beacon/src/example.ino
@@ -6,7 +6,7 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_PIN, HIGH);
-  delay(1000);
+  delay(100);
   digitalWrite(LED_PIN, LOW);
-  delay(1000);
+  delay(900);
 }
---
+++ 01_2_beacon/src/example.ino
@@ -0,0 +1,14 @@
+#define LED_PIN 13
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  delay(3000);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
